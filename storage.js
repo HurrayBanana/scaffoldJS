49c46be9dff6f5725c1608b1d4a5bf552e531064
Hurray Banana <HurrayBanana@users.noreply.github.com>
2022-10-21 12:54:01 +0100

    Create storage.js

---
+++ storage.js
@@ -0,0 +1,26 @@
+//declare a variable
+let a;
+
+//declare and initialise a variable
+let a = 5;
+
+//declare a constant - fixed value at design time must be initialised
+const dave = "bloke";
+
+//arrays
+//declare an empty array
+let arr = [];
+
+//create an 1d array instance of specific size 10;
+let arr = new Array(10)
+
+//initialise an empty array with 0
+for (let p = 0; p < 10; p++){
+  arr.push(0);
+}
+
+
+
+
+//append to an array - extend
+arr.push(item);
